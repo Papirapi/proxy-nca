389762a46ad4fdc7215823972cf75a0b313f0304
Badaoui Ahmed Badis <32328852+Papirapi@users.noreply.github.com>
2020-09-18 11:18:24 +0200

    Add files via upload

---
+++ foods_dataset.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import numpy as np
+
+path = "/content/foods/images"
+img_paths = []
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.jpg' in file:
+            files.append(os.path.join(r, file))
+
+for f in files:
+    d = (f.split("/",4)[-1])
+    img_paths.append(d)
+data = pd.DataFrame(img_paths)
+
+data[1]=(np.divmod(np.arange(len(data)),100)[0]+1)-1
+data.to_csv('/content/food.csv',index=False)
